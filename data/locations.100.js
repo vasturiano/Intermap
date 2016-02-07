b0d66b7d4cef6519b889cd76e54c0fe6e7a6b7ce
bhuffaker <bhuffake@gmail.com>
2016-02-06 17:39:21 -0800

    added a 100 version

---
+++ data/locations.100.js
@@ -0,0 +1,813 @@
+defined([], function {
+    return[
+   {
+      "population" : "417910",
+      "country" : "NZ",
+      "city" : "Auckland",
+      "lat" : "-36.86667",
+      "name" : "Auckland, NZ",
+      "lon" : "174.76667"
+   },
+   {
+      "population" : "363926",
+      "country" : "NZ",
+      "city" : "Christchurch",
+      "lat" : "-43.53333",
+      "name" : "Christchurch, NZ",
+      "lon" : "172.63333"
+   },
+   {
+      "population" : "187282",
+      "country" : "RU",
+      "city" : "Petropavlovsk-kamchatsky",
+      "lat" : "53.04444",
+      "name" : "Petropavlovsk-kamchatsky, RU",
+      "lon" : "158.65076"
+   },
+   {
+      "population" : "4627345",
+      "country" : "AU",
+      "city" : "Sydney",
+      "lat" : "-33.86785",
+      "name" : "Sydney, AU",
+      "lon" : "151.20732"
+   },
+   {
+      "population" : "367752",
+      "country" : "AU",
+      "city" : "Canberra",
+      "lat" : "-35.28346",
+      "name" : "Canberra, AU",
+      "lon" : "149.12807"
+   },
+   {
+      "population" : "4246375",
+      "country" : "AU",
+      "city" : "Melbourne",
+      "lat" : "-37.814",
+      "name" : "Melbourne, AU",
+      "lon" : "144.96332"
+   },
+   {
+      "population" : "1883027",
+      "country" : "JP",
+      "city" : "Sapporo",
+      "lat" : "43.06667",
+      "name" : "Sapporo, JP",
+      "lon" : "141.35"
+   },
+   {
+      "population" : "8336599",
+      "country" : "JP",
+      "city" : "Tokyo",
+      "lat" : "35.6895",
+      "name" : "Tokyo, JP",
+      "lon" : "139.69171"
+   },
+   {
+      "population" : "2592413",
+      "country" : "JP",
+      "city" : "Osaka",
+      "lat" : "34.69374",
+      "name" : "Osaka, JP",
+      "lon" : "135.50218"
+   },
+   {
+      "population" : "1392289",
+      "country" : "JP",
+      "city" : "Fukuoka",
+      "lat" : "33.6",
+      "name" : "Fukuoka, JP",
+      "lon" : "130.41667"
+   },
+   {
+      "population" : "10349312",
+      "country" : "KR",
+      "city" : "Seoul",
+      "lat" : "37.566",
+      "name" : "Seoul, KR",
+      "lon" : "126.9784"
+   },
+   {
+      "population" : "6255921",
+      "country" : "CN",
+      "city" : "Shenyang",
+      "lat" : "41.79222",
+      "name" : "Shenyang, CN",
+      "lon" : "123.43278"
+   },
+   {
+      "population" : "22315474",
+      "country" : "CN",
+      "city" : "Shanghai",
+      "lat" : "31.22222",
+      "name" : "Shanghai, CN",
+      "lon" : "121.45806"
+   },
+   {
+      "population" : "11716620",
+      "country" : "CN",
+      "city" : "Beijing",
+      "lat" : "39.9075",
+      "name" : "Beijing, CN",
+      "lon" : "116.39723"
+   },
+   {
+      "population" : "11071424",
+      "country" : "CN",
+      "city" : "Guangzhou",
+      "lat" : "23.11667",
+      "name" : "Guangzhou, CN",
+      "lon" : "113.25"
+   },
+   {
+      "population" : "6501190",
+      "country" : "CN",
+      "city" : "Xi'an",
+      "lat" : "34.25833",
+      "name" : "Xi'an, CN",
+      "lon" : "108.92861"
+   },
+   {
+      "population" : "8540121",
+      "country" : "ID",
+      "city" : "Jakarta",
+      "lat" : "-6.21462",
+      "name" : "Jakarta, ID",
+      "lon" : "106.84513"
+   },
+   {
+      "population" : "7415590",
+      "country" : "CN",
+      "city" : "Chengdu",
+      "lat" : "30.66667",
+      "name" : "Chengdu, CN",
+      "lon" : "104.06667"
+   },
+   {
+      "population" : "5104476",
+      "country" : "TH",
+      "city" : "Bangkok",
+      "lat" : "13.75398",
+      "name" : "Bangkok, TH",
+      "lon" : "100.50144"
+   },
+   {
+      "population" : "4477638",
+      "country" : "MM",
+      "city" : "Yangon",
+      "lat" : "16.80528",
+      "name" : "Yangon, MM",
+      "lon" : "96.15611"
+   },
+   {
+      "population" : "10356500",
+      "country" : "BD",
+      "city" : "Dhaka",
+      "lat" : "23.7104",
+      "name" : "Dhaka, BD",
+      "lon" : "90.40744"
+   },
+   {
+      "population" : "4631392",
+      "country" : "IN",
+      "city" : "Kolkata",
+      "lat" : "22.56263",
+      "name" : "Kolkata, IN",
+      "lon" : "88.36304"
+   },
+   {
+      "population" : "1599920",
+      "country" : "IN",
+      "city" : "Patna",
+      "lat" : "25.61538",
+      "name" : "Patna, IN",
+      "lon" : "85.10103"
+   },
+   {
+      "population" : "4328063",
+      "country" : "IN",
+      "city" : "Chennai",
+      "lat" : "13.08784",
+      "name" : "Chennai, IN",
+      "lon" : "80.27847"
+   },
+   {
+      "population" : "10927986",
+      "country" : "IN",
+      "city" : "Delhi",
+      "lat" : "28.65195",
+      "name" : "Delhi, IN",
+      "lon" : "77.23149"
+   },
+   {
+      "population" : "12691836",
+      "country" : "IN",
+      "city" : "Mumbai",
+      "lat" : "19.07283",
+      "name" : "Mumbai, IN",
+      "lon" : "72.88261"
+   },
+   {
+      "population" : "3043532",
+      "country" : "AF",
+      "city" : "Kabul",
+      "lat" : "34.52813",
+      "name" : "Kabul, AF",
+      "lon" : "69.17233"
+   },
+   {
+      "population" : "11624219",
+      "country" : "PK",
+      "city" : "Karachi",
+      "lat" : "24.9056",
+      "name" : "Karachi, PK",
+      "lon" : "67.0822"
+   },
+   {
+      "population" : "1062919",
+      "country" : "RU",
+      "city" : "Chelyabinsk",
+      "lat" : "55.15402",
+      "name" : "Chelyabinsk, RU",
+      "lon" : "61.42915"
+   },
+   {
+      "population" : "2307177",
+      "country" : "IR",
+      "city" : "Mashhad",
+      "lat" : "36.31559",
+      "name" : "Mashhad, IR",
+      "lon" : "59.56796"
+   },
+   {
+      "population" : "1137347",
+      "country" : "AE",
+      "city" : "Dubai",
+      "lat" : "25.0657",
+      "name" : "Dubai, AE",
+      "lon" : "55.17128"
+   },
+   {
+      "population" : "44099591",
+      "country" : "IR",
+      "city" : "Godarzi It",
+      "lat" : "35.73328",
+      "name" : "Godarzi It, IR",
+      "lon" : "51.30714"
+   },
+   {
+      "population" : "2600000",
+      "country" : "IQ",
+      "city" : "Basrah",
+      "lat" : "30.50852",
+      "name" : "Basrah, IQ",
+      "lon" : "47.7804"
+   },
+   {
+      "population" : "7216000",
+      "country" : "IQ",
+      "city" : "Baghdad",
+      "lat" : "33.34058",
+      "name" : "Baghdad, IQ",
+      "lon" : "44.40088"
+   },
+   {
+      "population" : "2065597",
+      "country" : "IQ",
+      "city" : "Al Mawsil Al Jadidah",
+      "lat" : "36.33306",
+      "name" : "Al Mawsil Al Jadidah, IQ",
+      "lon" : "43.1049"
+   },
+   {
+      "population" : "10381222",
+      "country" : "RU",
+      "city" : "Moscow",
+      "lat" : "55.75222",
+      "name" : "Moscow, RU",
+      "lon" : "37.61556"
+   },
+   {
+      "population" : "3517182",
+      "country" : "TR",
+      "city" : "Ankara",
+      "lat" : "39.91987",
+      "name" : "Ankara, TR",
+      "lon" : "32.85427"
+   },
+   {
+      "population" : "11174257",
+      "country" : "TR",
+      "city" : "Istanbul",
+      "lat" : "41.01384",
+      "name" : "Istanbul, TR",
+      "lon" : "28.94966"
+   },
+   {
+      "population" : "2500603",
+      "country" : "TR",
+      "city" : "Izmir",
+      "lat" : "38.41273",
+      "name" : "Izmir, TR",
+      "lon" : "27.13838"
+   },
+   {
+      "population" : "1152556",
+      "country" : "BG",
+      "city" : "Sofia",
+      "lat" : "42.69751",
+      "name" : "Sofia, BG",
+      "lon" : "23.32415"
+   },
+   {
+      "population" : "3433441",
+      "country" : "ZA",
+      "city" : "Cape Town",
+      "lat" : "-33.92584",
+      "name" : "Cape Town, ZA",
+      "lon" : "18.42322"
+   },
+   {
+      "population" : "7785965",
+      "country" : "CD",
+      "city" : "Kinshasa",
+      "lat" : "-4.32758",
+      "name" : "Kinshasa, CD",
+      "lon" : "15.31357"
+   },
+   {
+      "population" : "3426354",
+      "country" : "DE",
+      "city" : "Berlin",
+      "lat" : "52.52437",
+      "name" : "Berlin, DE",
+      "lon" : "13.41053"
+   },
+   {
+      "population" : "3626068",
+      "country" : "NG",
+      "city" : "Kano",
+      "lat" : "12.00012",
+      "name" : "Kano, NG",
+      "lon" : "8.51672"
+   },
+   {
+      "population" : "3565108",
+      "country" : "NG",
+      "city" : "Ibadan",
+      "lat" : "7.37756",
+      "name" : "Ibadan, NG",
+      "lon" : "3.90591"
+   },
+   {
+      "population" : "9000000",
+      "country" : "NG",
+      "city" : "Lagos",
+      "lat" : "6.45407",
+      "name" : "Lagos, NG",
+      "lon" : "3.39467"
+   },
+   {
+      "population" : "3677115",
+      "country" : "CI",
+      "city" : "Abidjan",
+      "lat" : "5.30966",
+      "name" : "Abidjan, CI",
+      "lon" : "-4.01266"
+   },
+   {
+      "population" : "3144909",
+      "country" : "MA",
+      "city" : "Casablanca",
+      "lat" : "33.58831",
+      "name" : "Casablanca, MA",
+      "lon" : "-7.61138"
+   },
+   {
+      "population" : "1871242",
+      "country" : "GN",
+      "city" : "Camayenne",
+      "lat" : "9.535",
+      "name" : "Camayenne, GN",
+      "lon" : "-13.68778"
+   },
+   {
+      "population" : "2476400",
+      "country" : "SN",
+      "city" : "Dakar",
+      "lat" : "14.6937",
+      "name" : "Dakar, SN",
+      "lon" : "-17.44406"
+   },
+   {
+      "population" : "118918",
+      "country" : "IS",
+      "city" : "Reykjavik",
+      "lat" : "64.13548",
+      "name" : "Reykjavik, IS",
+      "lon" : "-21.89541"
+   },
+   {
+      "population" : "1478098",
+      "country" : "BR",
+      "city" : "Recife",
+      "lat" : "-8.05389",
+      "name" : "Recife, BR",
+      "lon" : "-34.88111"
+   },
+   {
+      "population" : "2711840",
+      "country" : "BR",
+      "city" : "Salvador",
+      "lat" : "-12.97111",
+      "name" : "Salvador, BR",
+      "lon" : "-38.51083"
+   },
+   {
+      "population" : "744512",
+      "country" : "BR",
+      "city" : "Teresina",
+      "lat" : "-5.08917",
+      "name" : "Teresina, BR",
+      "lon" : "-42.80194"
+   },
+   {
+      "population" : "10021295",
+      "country" : "BR",
+      "city" : "Sao Paulo",
+      "lat" : "-23.5475",
+      "name" : "Sao Paulo, BR",
+      "lon" : "-46.63611"
+   },
+   {
+      "population" : "2207718",
+      "country" : "BR",
+      "city" : "Brasilia",
+      "lat" : "-15.77972",
+      "name" : "Brasilia, BR",
+      "lon" : "-47.92972"
+   },
+   {
+      "population" : "1718421",
+      "country" : "BR",
+      "city" : "Curitiba",
+      "lat" : "-25.42778",
+      "name" : "Curitiba, BR",
+      "lon" : "-49.27306"
+   },
+   {
+      "population" : "471832",
+      "country" : "BR",
+      "city" : "Londrina",
+      "lat" : "-23.31028",
+      "name" : "Londrina, BR",
+      "lon" : "-51.16278"
+   },
+   {
+      "population" : "1372741",
+      "country" : "BR",
+      "city" : "Porto Alegre",
+      "lat" : "-30.03306",
+      "name" : "Porto Alegre, BR",
+      "lon" : "-51.23"
+   },
+   {
+      "population" : "729151",
+      "country" : "BR",
+      "city" : "Campo Grande",
+      "lat" : "-20.44278",
+      "name" : "Campo Grande, BR",
+      "lon" : "-54.64639"
+   },
+   {
+      "population" : "1270737",
+      "country" : "UY",
+      "city" : "Montevideo",
+      "lat" : "-34.90328",
+      "name" : "Montevideo, UY",
+      "lon" : "-56.18816"
+   },
+   {
+      "population" : "13076300",
+      "country" : "AR",
+      "city" : "Buenos Aires",
+      "lat" : "-34.61315",
+      "name" : "Buenos Aires, AR",
+      "lon" : "-58.37723"
+   },
+   {
+      "population" : "1598210",
+      "country" : "BR",
+      "city" : "Manaus",
+      "lat" : "-3.10194",
+      "name" : "Manaus, BR",
+      "lon" : "-60.025"
+   },
+   {
+      "population" : "1364389",
+      "country" : "BO",
+      "city" : "Santa Cruz De La Sierra",
+      "lat" : "-17.78629",
+      "name" : "Santa Cruz De La Sierra, BO",
+      "lon" : "-63.18117"
+   },
+   {
+      "population" : "1428214",
+      "country" : "AR",
+      "city" : "Cordoba",
+      "lat" : "-31.4135",
+      "name" : "Cordoba, AR",
+      "lon" : "-64.18105"
+   },
+   {
+      "population" : "3000000",
+      "country" : "VE",
+      "city" : "Caracas",
+      "lat" : "10.48801",
+      "name" : "Caracas, VE",
+      "lon" : "-66.87919"
+   },
+   {
+      "population" : "1754256",
+      "country" : "VE",
+      "city" : "Maracay",
+      "lat" : "10.23535",
+      "name" : "Maracay, VE",
+      "lon" : "-67.59113"
+   },
+   {
+      "population" : "4837295",
+      "country" : "CL",
+      "city" : "Santiago",
+      "lat" : "-33.45694",
+      "name" : "Santiago, CL",
+      "lon" : "-70.64827"
+   },
+   {
+      "population" : "2225000",
+      "country" : "VE",
+      "city" : "Maracaibo",
+      "lat" : "10.66663",
+      "name" : "Maracaibo, VE",
+      "lon" : "-71.61245"
+   },
+   {
+      "population" : "8175133",
+      "country" : "US",
+      "city" : "New York City",
+      "lat" : "40.71427",
+      "name" : "New York City, US",
+      "lon" : "-74.00597"
+   },
+   {
+      "population" : "7674366",
+      "country" : "CO",
+      "city" : "Bogota",
+      "lat" : "4.60971",
+      "name" : "Bogota, CO",
+      "lon" : "-74.08175"
+   },
+   {
+      "population" : "3000000",
+      "country" : "JM",
+      "city" : "Fitzroy's Elecctronics",
+      "lat" : "17.98234",
+      "name" : "Fitzroy's Elecctronics, JM",
+      "lon" : "-76.86918"
+   },
+   {
+      "population" : "7737002",
+      "country" : "PE",
+      "city" : "Lima",
+      "lat" : "-12.04318",
+      "name" : "Lima, PE",
+      "lon" : "-77.02824"
+   },
+   {
+      "population" : "2600000",
+      "country" : "CA",
+      "city" : "Toronto",
+      "lat" : "43.70011",
+      "name" : "Toronto, CA",
+      "lon" : "-79.4163"
+   },
+   {
+      "population" : "2163824",
+      "country" : "CU",
+      "city" : "Havana",
+      "lat" : "23.13302",
+      "name" : "Havana, CU",
+      "lon" : "-82.38304"
+   },
+   {
+      "population" : "829718",
+      "country" : "US",
+      "city" : "Indianapolis",
+      "lat" : "39.76838",
+      "name" : "Indianapolis, US",
+      "lon" : "-86.15804"
+   },
+   {
+      "population" : "973087",
+      "country" : "NI",
+      "city" : "Managua",
+      "lat" : "12.13282",
+      "name" : "Managua, NI",
+      "lon" : "-86.2504"
+   },
+   {
+      "population" : "850848",
+      "country" : "HN",
+      "city" : "Tegucigalpa",
+      "lat" : "14.0818",
+      "name" : "Tegucigalpa, HN",
+      "lon" : "-87.20681"
+   },
+   {
+      "population" : "2695598",
+      "country" : "US",
+      "city" : "Chicago",
+      "lat" : "41.85003",
+      "name" : "Chicago, US",
+      "lon" : "-87.65005"
+   },
+   {
+      "population" : "646889",
+      "country" : "US",
+      "city" : "Memphis",
+      "lat" : "35.14953",
+      "name" : "Memphis, US",
+      "lon" : "-90.04898"
+   },
+   {
+      "population" : "994938",
+      "country" : "GT",
+      "city" : "Guatemala City",
+      "lat" : "14.64072",
+      "name" : "Guatemala City, GT",
+      "lon" : "-90.51327"
+   },
+   {
+      "population" : "2099451",
+      "country" : "US",
+      "city" : "Houston",
+      "lat" : "29.76328",
+      "name" : "Houston, US",
+      "lon" : "-95.36327"
+   },
+   {
+      "population" : "1197816",
+      "country" : "US",
+      "city" : "Dallas",
+      "lat" : "32.78306",
+      "name" : "Dallas, US",
+      "lon" : "-96.80667"
+   },
+   {
+      "population" : "1820888",
+      "country" : "MX",
+      "city" : "Iztapalapa",
+      "lat" : "19.35738",
+      "name" : "Iztapalapa, MX",
+      "lon" : "-99.0671"
+   },
+   {
+      "population" : "12294193",
+      "country" : "MX",
+      "city" : "Mexico City",
+      "lat" : "19.42847",
+      "name" : "Mexico City, MX",
+      "lon" : "-99.12766"
+   },
+   {
+      "population" : "1114626",
+      "country" : "MX",
+      "city" : "Leon",
+      "lat" : "21.13052",
+      "name" : "Leon, MX",
+      "lon" : "-101.671"
+   },
+   {
+      "population" : "1640589",
+      "country" : "MX",
+      "city" : "Guadalajara",
+      "lat" : "20.66682",
+      "name" : "Guadalajara, MX",
+      "lon" : "-103.39182"
+   },
+   {
+      "population" : "809232",
+      "country" : "MX",
+      "city" : "Chihuahua",
+      "lat" : "28.63528",
+      "name" : "Chihuahua, MX",
+      "lon" : "-106.08889"
+   },
+   {
+      "population" : "1512354",
+      "country" : "MX",
+      "city" : "Ciudad Juarez",
+      "lat" : "31.73333",
+      "name" : "Ciudad Juarez, MX",
+      "lon" : "-106.48333"
+   },
+   {
+      "population" : "520116",
+      "country" : "US",
+      "city" : "Tucson",
+      "lat" : "32.22174",
+      "name" : "Tucson, US",
+      "lon" : "-110.92648"
+   },
+   {
+      "population" : "595811",
+      "country" : "MX",
+      "city" : "Hermosillo",
+      "lat" : "29.1026",
+      "name" : "Hermosillo, MX",
+      "lon" : "-110.97732"
+   },
+   {
+      "population" : "1445632",
+      "country" : "US",
+      "city" : "Phoenix",
+      "lat" : "33.44838",
+      "name" : "Phoenix, US",
+      "lon" : "-112.07404"
+   },
+   {
+      "population" : "1019942",
+      "country" : "CA",
+      "city" : "Calgary",
+      "lat" : "51.05011",
+      "name" : "Calgary, CA",
+      "lon" : "-114.08529"
+   },
+   {
+      "population" : "1376457",
+      "country" : "MX",
+      "city" : "Tijuana",
+      "lat" : "32.5027",
+      "name" : "Tijuana, MX",
+      "lon" : "-117.00371"
+   },
+   {
+      "population" : "3792621",
+      "country" : "US",
+      "city" : "Los Angeles",
+      "lat" : "34.05223",
+      "name" : "Los Angeles, US",
+      "lon" : "-118.24368"
+   },
+   {
+      "population" : "945942",
+      "country" : "US",
+      "city" : "San Jose",
+      "lat" : "37.33939",
+      "name" : "San Jose, US",
+      "lon" : "-121.89496"
+   },
+   {
+      "population" : "608660",
+      "country" : "US",
+      "city" : "Seattle",
+      "lat" : "47.60621",
+      "name" : "Seattle, US",
+      "lon" : "-122.33207"
+   },
+   {
+      "population" : "805235",
+      "country" : "US",
+      "city" : "San Francisco",
+      "lat" : "37.77493",
+      "name" : "San Francisco, US",
+      "lon" : "-122.41942"
+   },
+   {
+      "population" : "600000",
+      "country" : "CA",
+      "city" : "Vancouver",
+      "lat" : "49.24966",
+      "name" : "Vancouver, CA",
+      "lon" : "-123.11934"
+   },
+   {
+      "population" : "291826",
+      "country" : "US",
+      "city" : "Anchorage",
+      "lat" : "61.21806",
+      "name" : "Anchorage, US",
+      "lon" : "-149.90028"
+   },
+   {
+      "population" : "371657",
+      "country" : "US",
+      "city" : "Honolulu",
+      "lat" : "21.30694",
+      "name" : "Honolulu, US",
+      "lon" : "-157.85833"
+   }
+]
+;
+});
