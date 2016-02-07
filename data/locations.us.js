acbe2845b36f241c6b127bc57de29b293dd0c036
bhuffaker <bhuffake@gmail.com>
2016-02-06 16:59:44 -0800

    added a us location file.

---
+++ data/locations.us.js
@@ -0,0 +1,712 @@
+defined([], function {
+    return[
+   {
+      "population" : "8175133",
+      "country" : "US",
+      "city" : "New York City",
+      "lat" : "40.71427",
+      "lon" : "-74.00597"
+   },
+   {
+      "population" : "3792621",
+      "country" : "US",
+      "city" : "Los Angeles",
+      "lat" : "34.05223",
+      "lon" : "-118.24368"
+   },
+   {
+      "population" : "2695598",
+      "country" : "US",
+      "city" : "Chicago",
+      "lat" : "41.85003",
+      "lon" : "-87.65005"
+   },
+   {
+      "population" : "2300664",
+      "country" : "US",
+      "city" : "Brooklyn",
+      "lat" : "40.6501",
+      "lon" : "-73.94958"
+   },
+   {
+      "population" : "2272771",
+      "country" : "US",
+      "city" : "Borough Of Queens",
+      "lat" : "40.68149",
+      "lon" : "-73.83652"
+   },
+   {
+      "population" : "2099451",
+      "country" : "US",
+      "city" : "Houston",
+      "lat" : "29.76328",
+      "lon" : "-95.36327"
+   },
+   {
+      "population" : "1526006",
+      "country" : "US",
+      "city" : "Philadelphia",
+      "lat" : "39.95233",
+      "lon" : "-75.16379"
+   },
+   {
+      "population" : "1445632",
+      "country" : "US",
+      "city" : "Phoenix",
+      "lat" : "33.44838",
+      "lon" : "-112.07404"
+   },
+   {
+      "population" : "1385108",
+      "country" : "US",
+      "city" : "The Bronx",
+      "lat" : "40.84985",
+      "lon" : "-73.86641"
+   },
+   {
+      "population" : "1327407",
+      "country" : "US",
+      "city" : "San Antonio",
+      "lat" : "29.42412",
+      "lon" : "-98.49363"
+   },
+   {
+      "population" : "1307402",
+      "country" : "US",
+      "city" : "San Diego",
+      "lat" : "32.71533",
+      "lon" : "-117.15726"
+   },
+   {
+      "population" : "1197816",
+      "country" : "US",
+      "city" : "Dallas",
+      "lat" : "32.78306",
+      "lon" : "-96.80667"
+   },
+   {
+      "population" : "945942",
+      "country" : "US",
+      "city" : "San Jose",
+      "lat" : "37.33939",
+      "lon" : "-121.89496"
+   },
+   {
+      "population" : "829718",
+      "country" : "US",
+      "city" : "Indianapolis",
+      "lat" : "39.76838",
+      "lon" : "-86.15804"
+   },
+   {
+      "population" : "821784",
+      "country" : "US",
+      "city" : "Jacksonville",
+      "lat" : "30.33218",
+      "lon" : "-81.65565"
+   },
+   {
+      "population" : "805235",
+      "country" : "US",
+      "city" : "San Francisco",
+      "lat" : "37.77493",
+      "lon" : "-122.41942"
+   },
+   {
+      "population" : "790390",
+      "country" : "US",
+      "city" : "Austin",
+      "lat" : "30.26715",
+      "lon" : "-97.74306"
+   },
+   {
+      "population" : "787033",
+      "country" : "US",
+      "city" : "Columbus",
+      "lat" : "39.96118",
+      "lon" : "-82.99879"
+   },
+   {
+      "population" : "741206",
+      "country" : "US",
+      "city" : "Fort Worth",
+      "lat" : "32.72541",
+      "lon" : "-97.32085"
+   },
+   {
+      "population" : "731424",
+      "country" : "US",
+      "city" : "Charlotte",
+      "lat" : "35.22709",
+      "lon" : "-80.84313"
+   },
+   {
+      "population" : "713777",
+      "country" : "US",
+      "city" : "Detroit",
+      "lat" : "42.33143",
+      "lon" : "-83.04575"
+   },
+   {
+      "population" : "649121",
+      "country" : "US",
+      "city" : "El Paso",
+      "lat" : "31.75872",
+      "lon" : "-106.48693"
+   },
+   {
+      "population" : "646889",
+      "country" : "US",
+      "city" : "Memphis",
+      "lat" : "35.14953",
+      "lon" : "-90.04898"
+   },
+   {
+      "population" : "641608",
+      "country" : "US",
+      "city" : "New South Memphis",
+      "lat" : "35.08676",
+      "lon" : "-90.05676"
+   },
+   {
+      "population" : "620961",
+      "country" : "US",
+      "city" : "Baltimore",
+      "lat" : "39.29038",
+      "lon" : "-76.61219"
+   },
+   {
+      "population" : "617594",
+      "country" : "US",
+      "city" : "Boston",
+      "lat" : "42.35843",
+      "lon" : "-71.05977"
+   },
+   {
+      "population" : "608660",
+      "country" : "US",
+      "city" : "Seattle",
+      "lat" : "47.60621",
+      "lon" : "-122.33207"
+   },
+   {
+      "population" : "601723",
+      "country" : "US",
+      "city" : "Washington, D.c.",
+      "lat" : "38.89511",
+      "lon" : "-77.03637"
+   },
+   {
+      "population" : "600158",
+      "country" : "US",
+      "city" : "Denver",
+      "lat" : "39.73915",
+      "lon" : "-104.9847"
+   },
+   {
+      "population" : "594833",
+      "country" : "US",
+      "city" : "Milwaukee",
+      "lat" : "43.0389",
+      "lon" : "-87.90647"
+   },
+   {
+      "population" : "583776",
+      "country" : "US",
+      "city" : "Portland",
+      "lat" : "45.52345",
+      "lon" : "-122.67621"
+   },
+   {
+      "population" : "583756",
+      "country" : "US",
+      "city" : "Las Vegas",
+      "lat" : "36.17497",
+      "lon" : "-115.13722"
+   },
+   {
+      "population" : "579999",
+      "country" : "US",
+      "city" : "Oklahoma City",
+      "lat" : "35.46756",
+      "lon" : "-97.51643"
+   },
+   {
+      "population" : "571281",
+      "country" : "US",
+      "city" : "South Boston",
+      "lat" : "42.33343",
+      "lon" : "-71.04949"
+   },
+   {
+      "population" : "545852",
+      "country" : "US",
+      "city" : "Albuquerque",
+      "lat" : "35.08449",
+      "lon" : "-106.65114"
+   },
+   {
+      "population" : "530852",
+      "country" : "US",
+      "city" : "Nashville",
+      "lat" : "36.16589",
+      "lon" : "-86.78444"
+   },
+   {
+      "population" : "520116",
+      "country" : "US",
+      "city" : "Tucson",
+      "lat" : "32.22174",
+      "lon" : "-110.92648"
+   },
+   {
+      "population" : "494665",
+      "country" : "US",
+      "city" : "Fresno",
+      "lat" : "36.74773",
+      "lon" : "-119.77237"
+   },
+   {
+      "population" : "468730",
+      "country" : "US",
+      "city" : "Staten Island",
+      "lat" : "40.56233",
+      "lon" : "-74.13986"
+   },
+   {
+      "population" : "466488",
+      "country" : "US",
+      "city" : "Sacramento",
+      "lat" : "38.58157",
+      "lon" : "-121.4944"
+   },
+   {
+      "population" : "462257",
+      "country" : "US",
+      "city" : "Long Beach",
+      "lat" : "33.76696",
+      "lon" : "-118.18923"
+   },
+   {
+      "population" : "459787",
+      "country" : "US",
+      "city" : "Kansas City",
+      "lat" : "39.09973",
+      "lon" : "-94.57857"
+   },
+   {
+      "population" : "441545",
+      "country" : "US",
+      "city" : "Intercontinental Kansas City At The Plaza",
+      "lat" : "39.03994",
+      "lon" : "-94.58763"
+   },
+   {
+      "population" : "439041",
+      "country" : "US",
+      "city" : "Mesa",
+      "lat" : "33.41253",
+      "lon" : "-111.83189"
+   },
+   {
+      "population" : "437994",
+      "country" : "US",
+      "city" : "Virginia Beach",
+      "lat" : "36.85293",
+      "lon" : "-75.97799"
+   },
+   {
+      "population" : "420003",
+      "country" : "US",
+      "city" : "Atlanta",
+      "lat" : "33.749",
+      "lon" : "-84.38798"
+   },
+   {
+      "population" : "416427",
+      "country" : "US",
+      "city" : "Colorado Springs",
+      "lat" : "38.83388",
+      "lon" : "-104.82136"
+   },
+   {
+      "population" : "408958",
+      "country" : "US",
+      "city" : "Omaha",
+      "lat" : "41.25861",
+      "lon" : "-95.93779"
+   },
+   {
+      "population" : "403892",
+      "country" : "US",
+      "city" : "Raleigh",
+      "lat" : "35.7721",
+      "lon" : "-78.63861"
+   },
+   {
+      "population" : "399457",
+      "country" : "US",
+      "city" : "Miami",
+      "lat" : "25.77427",
+      "lon" : "-80.19366"
+   },
+   {
+      "population" : "396815",
+      "country" : "US",
+      "city" : "Cleveland",
+      "lat" : "41.4995",
+      "lon" : "-81.69541"
+   },
+   {
+      "population" : "391906",
+      "country" : "US",
+      "city" : "Tulsa",
+      "lat" : "36.15398",
+      "lon" : "-95.99277"
+   },
+   {
+      "population" : "390724",
+      "country" : "US",
+      "city" : "Oakland",
+      "lat" : "37.80437",
+      "lon" : "-122.2708"
+   },
+   {
+      "population" : "382578",
+      "country" : "US",
+      "city" : "Minneapolis",
+      "lat" : "44.97997",
+      "lon" : "-93.26384"
+   },
+   {
+      "population" : "382368",
+      "country" : "US",
+      "city" : "Wichita",
+      "lat" : "37.69224",
+      "lon" : "-97.33754"
+   },
+   {
+      "population" : "371657",
+      "country" : "US",
+      "city" : "Honolulu",
+      "lat" : "21.30694",
+      "lon" : "-157.85833"
+   },
+   {
+      "population" : "365438",
+      "country" : "US",
+      "city" : "Arlington",
+      "lat" : "32.73569",
+      "lon" : "-97.10807"
+   },
+   {
+      "population" : "347483",
+      "country" : "US",
+      "city" : "Bakersfield",
+      "lat" : "35.37329",
+      "lon" : "-119.01871"
+   },
+   {
+      "population" : "343829",
+      "country" : "US",
+      "city" : "New Orleans",
+      "lat" : "29.95465",
+      "lon" : "-90.07507"
+   },
+   {
+      "population" : "338759",
+      "country" : "US",
+      "city" : "West Raleigh",
+      "lat" : "35.78682",
+      "lon" : "-78.66389"
+   },
+   {
+      "population" : "336265",
+      "country" : "US",
+      "city" : "Anaheim",
+      "lat" : "33.83529",
+      "lon" : "-117.9145"
+   },
+   {
+      "population" : "335709",
+      "country" : "US",
+      "city" : "Tampa",
+      "lat" : "27.94752",
+      "lon" : "-82.45843"
+   },
+   {
+      "population" : "325078",
+      "country" : "US",
+      "city" : "Aurora",
+      "lat" : "39.72943",
+      "lon" : "-104.83192"
+   },
+   {
+      "population" : "324528",
+      "country" : "US",
+      "city" : "Santa Ana",
+      "lat" : "33.74557",
+      "lon" : "-117.86783"
+   },
+   {
+      "population" : "319294",
+      "country" : "US",
+      "city" : "St. Louis",
+      "lat" : "38.62727",
+      "lon" : "-90.19789"
+   },
+   {
+      "population" : "305704",
+      "country" : "US",
+      "city" : "Pittsburgh",
+      "lat" : "40.44062",
+      "lon" : "-79.99589"
+   },
+   {
+      "population" : "305215",
+      "country" : "US",
+      "city" : "Corpus Christi",
+      "lat" : "27.80058",
+      "lon" : "-97.39638"
+   },
+   {
+      "population" : "303871",
+      "country" : "US",
+      "city" : "Riverside",
+      "lat" : "33.95335",
+      "lon" : "-117.39616"
+   },
+   {
+      "population" : "296943",
+      "country" : "US",
+      "city" : "Cincinnati",
+      "lat" : "39.162",
+      "lon" : "-84.45689"
+   },
+   {
+      "population" : "295803",
+      "country" : "US",
+      "city" : "Lexington-fayette",
+      "lat" : "38.0498",
+      "lon" : "-84.45855"
+   },
+   {
+      "population" : "291826",
+      "country" : "US",
+      "city" : "Anchorage",
+      "lat" : "61.21806",
+      "lon" : "-149.90028"
+   },
+   {
+      "population" : "291707",
+      "country" : "US",
+      "city" : "Stockton",
+      "lat" : "37.9577",
+      "lon" : "-121.29078"
+   },
+   {
+      "population" : "288649",
+      "country" : "US",
+      "city" : "Ironville",
+      "lat" : "38.45647",
+      "lon" : "-82.69238"
+   },
+   {
+      "population" : "288649",
+      "country" : "US",
+      "city" : "Meads",
+      "lat" : "38.41258",
+      "lon" : "-82.70905"
+   },
+   {
+      "population" : "287208",
+      "country" : "US",
+      "city" : "Toledo",
+      "lat" : "41.66394",
+      "lon" : "-83.55521"
+   },
+   {
+      "population" : "285068",
+      "country" : "US",
+      "city" : "Saint Paul",
+      "lat" : "44.94441",
+      "lon" : "-93.09327"
+   },
+   {
+      "population" : "277140",
+      "country" : "US",
+      "city" : "Newark",
+      "lat" : "40.73566",
+      "lon" : "-74.17237"
+   },
+   {
+      "population" : "269666",
+      "country" : "US",
+      "city" : "Greensboro",
+      "lat" : "36.07264",
+      "lon" : "-79.79198"
+   },
+   {
+      "population" : "261310",
+      "country" : "US",
+      "city" : "Buffalo",
+      "lat" : "42.88645",
+      "lon" : "-78.87837"
+   },
+   {
+      "population" : "259841",
+      "country" : "US",
+      "city" : "Plano",
+      "lat" : "33.01984",
+      "lon" : "-96.69889"
+   },
+   {
+      "population" : "258379",
+      "country" : "US",
+      "city" : "Lincoln",
+      "lat" : "40.8",
+      "lon" : "-96.66696"
+   },
+   {
+      "population" : "257729",
+      "country" : "US",
+      "city" : "Henderson",
+      "lat" : "36.0397",
+      "lon" : "-114.98194"
+   },
+   {
+      "population" : "253691",
+      "country" : "US",
+      "city" : "Fort Wayne",
+      "lat" : "41.1306",
+      "lon" : "-85.12886"
+   },
+   {
+      "population" : "247597",
+      "country" : "US",
+      "city" : "Jersey City",
+      "lat" : "40.72816",
+      "lon" : "-74.07764"
+   },
+   {
+      "population" : "244769",
+      "country" : "US",
+      "city" : "Saint Petersburg",
+      "lat" : "27.77086",
+      "lon" : "-82.67927"
+   },
+   {
+      "population" : "243916",
+      "country" : "US",
+      "city" : "Chula Vista",
+      "lat" : "32.64005",
+      "lon" : "-117.0842"
+   },
+   {
+      "population" : "243639",
+      "country" : "US",
+      "city" : "Louisville",
+      "lat" : "38.25424",
+      "lon" : "-85.75941"
+   },
+   {
+      "population" : "242803",
+      "country" : "US",
+      "city" : "Norfolk",
+      "lat" : "36.84681",
+      "lon" : "-76.28522"
+   },
+   {
+      "population" : "238300",
+      "country" : "US",
+      "city" : "Orlando",
+      "lat" : "28.53834",
+      "lon" : "-81.37924"
+   },
+   {
+      "population" : "236123",
+      "country" : "US",
+      "city" : "Chandler",
+      "lat" : "33.30616",
+      "lon" : "-111.84125"
+   },
+   {
+      "population" : "236091",
+      "country" : "US",
+      "city" : "Laredo",
+      "lat" : "27.50641",
+      "lon" : "-99.50754"
+   },
+   {
+      "population" : "233209",
+      "country" : "US",
+      "city" : "Madison",
+      "lat" : "43.07305",
+      "lon" : "-89.40123"
+   },
+   {
+      "population" : "229617",
+      "country" : "US",
+      "city" : "Winston-salem",
+      "lat" : "36.09986",
+      "lon" : "-80.24422"
+   },
+   {
+      "population" : "229573",
+      "country" : "US",
+      "city" : "Lubbock",
+      "lat" : "33.57786",
+      "lon" : "-101.85517"
+   },
+   {
+      "population" : "229493",
+      "country" : "US",
+      "city" : "Baton Rouge",
+      "lat" : "30.45075",
+      "lon" : "-91.15455"
+   },
+   {
+      "population" : "228330",
+      "country" : "US",
+      "city" : "Durham",
+      "lat" : "35.99403",
+      "lon" : "-78.89862"
+   },
+   {
+      "population" : "226876",
+      "country" : "US",
+      "city" : "Garland",
+      "lat" : "32.91262",
+      "lon" : "-96.63888"
+   },
+   {
+      "population" : "226721",
+      "country" : "US",
+      "city" : "Glendale",
+      "lat" : "33.53865",
+      "lon" : "-112.18599"
+   },
+   {
+      "population" : "225366",
+      "country" : "US",
+      "city" : "Lexington",
+      "lat" : "37.98869",
+      "lon" : "-84.47772"
+   },
+   {
+      "population" : "225221",
+      "country" : "US",
+      "city" : "Reno",
+      "lat" : "39.52963",
+      "lon" : "-119.8138"
+   },
+   {
+      "population" : "224669",
+      "country" : "US",
+      "city" : "Hialeah",
+      "lat" : "25.8576",
+      "lon" : "-80.27811"
+   }
+]
+;
+});
