cb33b94f609c40174fb2f49992c7929f78968491
vasco <vastur@gmail.com>
2016-02-17 01:30:33 -0800

    Remove cdn vendor dependencies

--- require-config.js
+++ require-config.js
@@ -1,16 +1,14 @@
 require.config({
     paths: {
         babel: '//cdnjs.cloudflare.com/ajax/libs/babel-core/5.8.34/browser.min',
-        jsx: 'vendor/jsx',
-        //jsx: '//cdn.rawgit.com/podio/requirejs-react-jsx/v1.0.2/jsx',
+        jsx: '//cdn.rawgit.com/podio/requirejs-react-jsx/v1.0.2/jsx',
         text: '//cdnjs.cloudflare.com/ajax/libs/require-text/2.0.12/text.min',
 
         react: '//cdnjs.cloudflare.com/ajax/libs/react/0.14.6/react-with-addons.min',
         'react-dom': '//cdnjs.cloudflare.com/ajax/libs/react/0.14.6/react-dom.min',
         'react-bootstrap': '//cdnjs.cloudflare.com/ajax/libs/react-bootstrap/0.28.1/react-bootstrap.min',
 
-        //jquery: '//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min',
-        jquery: 'vendor/jquery.min',
+        jquery: '//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min',
         bootstrap: '//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min',
         underscore: '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min',
         moment: '//cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.0/moment.min',
