fea6c89e4779a36cf4fb76a8406cbc6a106aa1f4
vasco <vastur@gmail.com>
2016-03-01 00:16:31 -0800

    Turn city population to int

--- locations.js
+++ locations.js
@@ -3,7 +3,7 @@ define(['data/locations.100'], function(locs) {
         return {
             text: loc.city + ' (' + loc.country + ')',
             angle: loc.lon,
-            weight: loc.population
+            weight: +loc.population
         };
     }).sort(function(a,b) {
         return b.weight-a.weight;
